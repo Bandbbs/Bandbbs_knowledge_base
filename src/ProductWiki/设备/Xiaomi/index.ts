93f700379d77b82bcc5a9eec67d902f0ccf15050
Zaona <3409473369@qq.com>
2026-03-14 00:09:58 +0800

    docs: 新增文章设备参数与设备命名

--- src/ProductWiki/设备/Xiaomi/index.ts
+++ src/ProductWiki/设备/Xiaomi/index.ts
@@ -3,5 +3,17 @@ export default [
         text: '手表',
         collapsed: true,
         items: []
+    },
+    {
+        text: '其他说明',
+        collapsed: true,
+        items: [{
+            text: '设备参数',
+            link: '/ProductWiki/设备/Xiaomi/device'
+        }, {
+            text: '设备命名',
+            link: '/ProductWiki/设备/Xiaomi/name'
+        },
+        ]
     }
 ]
